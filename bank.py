6a36272805dd0e0eee2e2da4a96c64be9f05972c
jarvisru <dar58896123@gmail.com>
2017-06-28 15:18:20 +0800

    modify bank.py to get time now

--- bank.py
+++ bank.py
@@ -1,15 +1,18 @@
 # include necessary package
-# need to install package [pandas lxml html5lib / openpyxl]
+# need to install package [pandas lxml html5lib / datetime / openpyxl]
 import pandas
 import html5lib
-
+from datetime import datetime
 # initial set
 dfs = pandas.read_html('http://rate.bot.com.tw/xrt?Lang=zh-TW')
 dataFrame = dfs[0]
 
-# get 0~5 column of data 
+# get 0~5 columns of data 
 fiveCol = dataFrame.ix[:,0:5]
 # set column name and set as unicode
 fiveCol.columns = [u'幣別',u'現金匯率-行買入',u'現金匯率-本行賣出',u'即期匯率-本行買入',u'即期匯率-本行賣出']
-# save as excel file
+# save time now
+fiveCol['時間'] = datetime.now().strftime('%Y-%m-%d')
+
+# # save as excel file
 fiveCol.to_excel('bank.xlsx')
